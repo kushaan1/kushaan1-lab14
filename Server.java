a6dee8717af9048f6303971a29f150f0916fb177
kushaan1 <kushaan.nagpal02@gmail.com>
2025-11-13 00:35:04 -0500

    Cleaned up code to throw numberformatexception, removed unnecessary println statements

--- Server.java
+++ Server.java
@@ -5,7 +5,7 @@ import java.util.*;
 
 public class Server {
     private ServerSocket serverSocket;
-    private final List<LocalDateTime> connectedTimes = new ArrayList<>();
+    private final ArrayList<LocalDateTime> connectedTimes = new ArrayList<>();
 
     public Server(int port) throws IOException {
         this.serverSocket = new ServerSocket(port);
@@ -22,23 +22,64 @@ public class Server {
         }
     }
 
-    public List<LocalDateTime> getConnectedTimes() {
+    public ArrayList<LocalDateTime> getConnectedTimes() {
         ArrayList<LocalDateTime> sorted = new ArrayList<>(connectedTimes);
         Collections.sort(sorted);
         return sorted;
     }
 
-    private void handleClient(Socket clientSocket) {
-    };
+    private class clientThread extends Thread {
+        private Socket clientSocket;
 
-    private int factorize(long number) {
+        public clientThread(Socket socket) {
+            this.clientSocket = socket;
+        }
+
+        public void run() {
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                    PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)) {
+
+                String handshake = in.readLine();
+                if (!"12345".equals(handshake)) {
+                    out.println("couldn't handshake");
+                    clientSocket.close();
+                    return;
+                }
+
+                String line;
+                while ((line = in.readLine()) != null) {
+                    try {
+                        int number = Integer.parseInt(line);
+                        int factors = factorize(number);
+                        out.println("The number " + number + " has " + factors + " factors");
+                    } catch (NumberFormatException e) {
+                        out.println("There was an exception on the server");
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    clientSocket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private int factorize(int number) {
         int count = 0;
-        for (long i = 1; i <= number; i++) {
+        for (int i = 1; i <= number; i++) {
             if (number % i == 0) {
                 count++;
             }
         }
         return count;
+    }
+
+    public void disconnect() throws IOException {
+        this.serverSocket.close();
     }
 
 }
