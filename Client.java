62959e492f5e835f5ef8a56aed9f7fd35dfd067d
kushaan1 <kushaan.nagpal02@gmail.com>
2025-11-12 23:27:20 -0500

    Created logic for client class

--- Client.java
+++ Client.java
@@ -1,3 +1,25 @@
+import java.io.IOException;
+import java.net.Socket;
+
 public class Client {
+    private Socket socket;
+
+    public Client(String host, int port) throws IOException {
+
+        this.socket = new Socket(host, port);
+    }
+
+    public Socket getSocket() {
+        return this.socket;
+    }
+
+    public void handshake() throws IOException {
+        this.socket.getOutputStream().write("12345\n".getBytes());
+        this.socket.getOutputStream().flush();
+    }
+
+    public void disconnect() throws IOException {
+        this.socket.close();
+    }
 
 }
