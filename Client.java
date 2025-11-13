7f9418bc28489b2a06e28e5ffd14ad81a8d6d8ef
kushaan1 <kushaan.nagpal02@gmail.com>
2025-11-12 23:39:13 -0500

    Updated client class functionality to handle failing test cases

--- Client.java
+++ Client.java
@@ -1,12 +1,19 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Client {
     private Socket socket;
+    private BufferedReader in;
+    private PrintWriter out;
 
     public Client(String host, int port) throws IOException {
 
         this.socket = new Socket(host, port);
+        this.in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        this.out = new PrintWriter(socket.getOutputStream(), true);
     }
 
     public Socket getSocket() {
@@ -14,8 +21,21 @@ public class Client {
     }
 
     public void handshake() throws IOException {
-        this.socket.getOutputStream().write("12345\n".getBytes());
-        this.socket.getOutputStream().flush();
+        out.println("12345");
+        out.flush();
+
+        String response = in.readLine();
+        if (!"OK".equals(response)) {
+            throw new IOException("Handshake failed: " + response);
+        }
+    }
+
+    public String request(String number) throws IOException {
+        out.println(number);
+        out.flush();
+
+        return in.readLine();
+
     }
 
     public void disconnect() throws IOException {
